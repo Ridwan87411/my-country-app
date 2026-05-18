f64bfcc382d7f0350d6c6936f6710e95944da377
Ridwan87411 <ridwansadib7@gmail.com>
2026-05-18 23:09:33 +0600

    Visit country

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,7 +1,38 @@
-const Countries = () => {
+import { use, useState } from "react";
+import Country from "../Country/Country";
+import './Countries.css';
+
+const Countries = ({ countriesPromise }) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const handleVisitedCountries = (country) => {
+        console.log('Handle visited country clicked' ,country);
+        const newVisitedCountries = [...visitedCountries , country];
+        setVisitedCountries(newVisitedCountries);
+        
+    };
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+    
     return (
-        <div>
-            <h2>Countries Details.......</h2>
+        <div >
+            
+            <h2>Countries Details : {countries.length}</h2>
+            <h2>Total Country Visited: {visitedCountries.length}</h2>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+                }
+            </ol>
+            <div className="countries">
+            {
+            countries.map(country => <Country
+                key={country.cca3.cca3}
+                country={country}
+                handleVisitedCountries={handleVisitedCountries}
+                ></Country>)
+
+            }
+            </div>
         </div>
     );
 };
